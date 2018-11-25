b4efff99e52971c5422bda431fb4522b42b9bf2b
Leszek Macura <leszek@macura.co>
2018-11-25 20:14:00 +0100

    Zadanie 17_03 poprawa1

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
-
 var app = express();
 var stringifyFile;
 
@@ -13,14 +12,6 @@ fs.readFile('./test.json', 'utf-8', function (err, data) {
     console.log(stringifyFile);
 });
 
-
-app.get('/getNote', function (req, res) {
-    fs.readFile('./test.json', 'utf-8', function (err, data) {
-        if (err) throw err;
-        stringifyFile = data;
-        res.send(data);
-    });
-});
 
 app.post('/updateNote/:note', function (req, res) {
 
@@ -35,10 +26,18 @@ app.post('/updateNote/:note', function (req, res) {
     res.send('Zawartość test.json po dodaniu notatki: ' + stringifyFile);
 });
 
+app.get('/getNote', function (req, res) {
+    fs.readFile('./test.json', 'utf-8', function (err, data) {
+        if (err) throw err;
+        stringifyFile = data;
+        res.send(data);
+    });
+});
+
 
 var server = app.listen(3000);
 
-// app.use(function (req, res, next) {
-//     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego czego żądasz :-(')
-// });
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego czego żądasz :-(')
+});
 
