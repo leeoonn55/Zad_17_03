ded0543a0348dd80e28557d2e17d87074a603a54
Leszek Macura <leszek@macura.co>
2018-11-26 22:12:01 +0100

    Zadanie 17_03 gotowe:-)

--- server.js
+++ server.js
@@ -6,33 +6,36 @@ var stringifyFile;
 
 app.use(bodyParser.json());
 
-fs.readFile('./test.json', 'utf-8', function (err, data) {
-    if (err) throw err;
-    stringifyFile = data;
-    console.log(stringifyFile);
-});
-
-
 app.post('/updateNote/:note', function (req, res) {
 
-    parsedFile = JSON.parse(stringifyFile);
-    parsedFile.note = req.params.note;
-    stringifyFile = JSON.stringify(parsedFile);
 
-    fs.writeFile('./test.json', stringifyFile, function (err) {
-        if (err) throw err;
-        console.log('file updated');
-    });
-    res.send('Zawartość test.json po dodaniu notatki: ' + stringifyFile);
-});
-
-app.get('/getNote', function (req, res) {
     fs.readFile('./test.json', 'utf-8', function (err, data) {
         if (err) throw err;
         stringifyFile = data;
-        res.send(data);
+
+        parsedFile = JSON.parse(stringifyFile);
+        parsedFile.note = req.params.note;
+        stringifyFile = JSON.stringify(parsedFile);
+
+        fs.writeFile('./test.json', stringifyFile, function (err) {
+            if (err) throw err;
+            console.log('file updated');
+        });
+
+        res.send('Zawartość test.json po dodaniu notatki: ' + stringifyFile);
+    });      
     });
-});
+
+    
+    app.get('/getNote', function (req, res) {
+        fs.readFile('./test.json', 'utf-8', function (err, data) {
+            if (err) throw err;
+            stringifyFile = data;
+            res.send(data);
+        });
+    });
+
+
 
 
 var server = app.listen(3000);
